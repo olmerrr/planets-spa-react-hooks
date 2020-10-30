4bc4d65c00b73c62200a8fc8263d6f67780cc7ad
Yaroslav <dombrovskiy91@gmail.com>
2020-10-30 16:37:19 +0200

    take date from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
-import React from 'react';
-import {Link, BrowserRouter as Router} from 'react-router-dom';
+import React,{useState, useEffect} from 'react';
+import {Link, BrowserRouter as Router, Route} from 'react-router-dom';
+
+const HomePage = ()=> {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] =useState();
+  
+  useEffect( () =>{
+    fetch('https://swapi.dev/api/planets/', {})
+      .then((res) => res.json())
+      .then((response) => {
+        setData(response.results);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      {
+        !isLoading && 
+          data.map((planet, index) => {
+            return <h2 key = {index}>{planet.name}</h2>
+          })
+      }
+    </div>
+  )
+}
 const App = () => {
-  return <p>Clean project</p>
+  return (
+    <div>
+      <Router>
+        <Route exact path = "/" component = {HomePage}/>
+      </Router>
+    </div>
+  )
 }
 export default App;
