e38a3492d31fa155e15db682b34ab96094f8ed38
Yaroslav <dombrovskiy91@gmail.com>
2020-11-01 19:24:36 +0200

    planetId  match params, rendering date

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
-import React,{useState, useEffect} from 'react';
-import {Link, BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
 
-const PlanetPage = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState();
-
-    useEffect(() => {
-      fetch(`https://swapi.dev/api/planets/${planetId}`, {})
-      .then((res)=> res.json())
-      .then((response)=> {
-        setData(response);
-        setIsLoading(false);
-        console.log(`https://swapi.dev/api/planets/${planetId}`)
-      })
-      .catch((error) => console.log(error))
-    },[planetId]);
-  
-    return(
-      <div>
-        {isLoading && (
-          <div>
-            <h2>{data.name}</h2>
-            <p>{data.rotation_period}</p>
-            <p>{data.diameter}</p>
-            <p>{data.climate}</p>
-            <p>{data.gravity}</p>
-            <p>{data.terrain}</p>
-            <p>{data.population}</p>
-            <p>{data.residents}</p>
-
-          <Link  to="/">Go back to Home Page</Link>
-          </div>
-        )}
-      </div>
-    )
-}
-
-const HomePage = ()=> {
+const PersonPage = ({ match }) => {
+  const {
+    params: { planetId },
+  } = match;
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState();
-  
-  useEffect( () =>{
-    fetch('https://swapi.dev/api/planets/', {})
+
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/planets/${planetId}`, {})
+      .then((res) => res.json())
+      .then((response) => {
+        setData(response)
+        setIsLoading(false);
+        console.log(`https://swapi.dev/api/planets/${planetId}`);
+      })
+      .catch((error) => console.log(error));
+  }, [planetId]);
+
+  return (
+    <>
+      {!isLoading && (
+        <>
+          <h2>{data.name}</h2>
+          <p>{data.rotation_period}</p>
+          <p>{data.diameter}</p>
+          <p>{data.climate}</p>
+          <p>{data.gravity}</p>
+          <p>{data.terrain}</p>
+          <p>{data.population}</p>
+          <p>{data.residents.map(residents => <p>{residents}</p>)}</p>
+
+          <Link to="/">Back to homepage</Link>
+        </>
+      )}
+    </>
+  );
+};
+
+const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/planets/", {})
       .then((res) => res.json())
       .then((response) => {
         setData(response.results);
@@ -51,27 +55,28 @@ const HomePage = ()=> {
   }, []);
 
   return (
-    <div>
-      {
-        !isLoading && 
-          data.map((planet, index) => {
-            return <h2 key = {index}>
-              <Link to = {`/planet/${index + 1}`}>{planet.name}</Link>
-              </h2>
-          })
-      }
-    </div>
-  )
-}
+    <>
+      {!isLoading &&
+        data.map((planet, index) => {
+          return (
+            <h5 key={index}>
+              <Link to={`/planet/${index + 1}`}>{planet.name}'s Page</Link>
+            </h5>
+          );
+        })}
+    </>
+  );
+};
+
 const App = () => {
   return (
-    <div>
+    <>
       <Router>
-        <Route exact path = "/" component = {HomePage}/>
-        <Route exact path = "/planet/:planetId" component = {PlanetPage}/>
-
+        <Route exact path="/" component={HomePage} />
+        <Route path="/planet/:planetId" component={PersonPage} />
       </Router>
-    </div>
-  )
-}
+    </>
+  );
+};
+
 export default App;
